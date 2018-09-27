f5eaac249a4535470825e8b964e5423908f7678c
daheige <zhuwei313@hotmail.com>
2018-09-27 23:55:38 +0800

    http go func运用

--- hg-gin/application/controller/HomeController.go
+++ hg-gin/application/controller/HomeController.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"fmt"
+	"log"
 	"thinkgo/common"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gomodule/redigo/redis"
@@ -32,6 +34,41 @@ func (ctrl *HomeController) Index(ctx *gin.Context) {
 }
 
 func (ctrl *HomeController) Test(ctx *gin.Context) {
+	//通过goroutine独立运行
+	newCtx := ctx.Copy()
+	go func() {
+		// time.Sleep(12 * time.Second)
+		// log.Println(newCtx)
+		log.Println(newCtx.ClientIP())
+	}()
+
+	go func() {
+		client := common.GetRedisClient("default")
+		defer client.Close()
+
+		log.Println(client.Do("hsetnx", "myhash", "daheige", time.Now().Format("2016-01-02 15:04:05")))
+		//设置过期时间
+
+		timeStr := time.Now().Format("2006-01-02")
+		fmt.Println("timeStr:", timeStr)
+		t, _ := time.Parse("2006-01-02", timeStr)
+		timeNumber := t.Unix()
+		fmt.Println("timeNumber:", timeNumber)
+
+		//获取本地location
+		toBeCharge := timeStr                                           //待转化为时间戳的字符串 注意 这里的小时和分钟还要秒必须写 因为是跟着模板走的 修改模板的话也可以不写
+		timeLayout := "2006-01-02"                                      //转化所需模板
+		loc, _ := time.LoadLocation("PRC")                              //重要：获取时区
+		theTime, _ := time.ParseInLocation(timeLayout, toBeCharge, loc) //使用模板在对应时区转化为time.time类型
+		sr := theTime.Unix()                                            //转化为时间戳 类型是int64
+		fmt.Println(theTime)                                            //打印输出theTime 2015-01-01 15:15:00 +0800 CST
+		fmt.Println(sr)
+
+		client.Do("expireAt", "myhash", sr+86400*3) //设置过期时间
+		log.Println("do once")
+		log.Println("xxx")
+	}()
+
 	ctx.String(200, "this is test page %s", "daheige")
 }
 
